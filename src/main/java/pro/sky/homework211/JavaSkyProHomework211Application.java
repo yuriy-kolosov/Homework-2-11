995c3c0389ad0304220ca65001b751e27500ddeb
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-04-08 21:16:44 +0300

    Домашнее задание 2-11. Библиотеки - начальный коммит

---
+++ src/main/java/pro/sky/homework211/JavaSkyProHomework211Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSkyProHomework211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSkyProHomework211Application.class, args);
+	}
+
+}
